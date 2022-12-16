eb4951eebf3d9afaa36280b7e2bcce4d77a3bcb2
Tigran O. Saatchyan <mr.saatchyan@yandex.com>
2022-12-16 15:36:40 +0400

    added card template / list of candidates template

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template
+import os
 
+from flask import Flask, render_template, request
+
+import utils
 from utils import ALL_CANDIDATES
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24).hex()
 
 
 @app.route('/')
@@ -18,6 +22,27 @@ def page_list():
     }
 
     return render_template('list.html', data=candidates)
+
+
+@app.route('/candidate/<int:candidate_id>/')
+def page_candidate_by_id(candidate_id):
+    candidate = utils.get_candidate(candidate_id)
+    return render_template("card.html", data=candidate)
+
+
+@app.route('/search_result/', methods=['POST'])
+def page_search_result():
+    candidate_name = None
+
+    if request.method == 'POST':
+        candidate_name = request.form['search']
+
+    if not candidate_name:
+        candidates = [{"error": "Name of candidate is required"}]
+    else:
+        candidates = utils.get_candidates_by_name(candidate_name)
+
+    return render_template("search_result.html", data=candidates)
 
 
 app.run(debug=True)
--- utils.py
+++ utils.py
@@ -18,22 +18,44 @@ def list_of_candidates():
 
 
 def get_candidate(candidate_id):
+    result = []
     for candidate in ALL_CANDIDATES:
-        if candidate["id"] == candidate_id:
-            return candidate
-        else:
-            return {"error": f"Candidate #{candidate_id} is missing"}
+        if candidate["id"] == int(candidate_id):
+            result = candidate
+
+    if not result:
+        result.append({"error": f"Candidate #{candidate_id} is missing"})
+
+    return result
 
 
 def get_candidates_by_name(candidate_name):
-    filtered_candidates = {}
+    filter_by_name = []
     for candidate in ALL_CANDIDATES:
-        if candidate_name.lower() == candidate["name"].lower():
-            filtered_candidates[candidate["id"]] = candidate["name"]
-    if filtered_candidates is {}:
-        filtered_candidates["error"] = f"Candidate {candidate_name} is missing"
-    return filtered_candidates
+        if candidate_name.lower().strip() in candidate["name"].lower().split():
+            filter_by_name.append(candidate)
+
+    if not filter_by_name:
+        filter_by_name.append(
+            {
+                "error": f"Candidate {candidate_name} is missing"
+            }
+        )
+    return filter_by_name
 
 
 def get_candidates_by_skill(skill_name):
-    pass
+    filter_by_skill = []
+    for candidate in ALL_CANDIDATES:
+        if skill_name.lower().strip() in candidate["skills"].lower().split():
+            print(skill_name.lower())
+            print(candidate["skills"].lower().split())
+            filter_by_skill.append(candidate)
+
+    if not filter_by_skill:
+        filter_by_skill.append(
+            {
+                "error": f"Candidate with {skill_name} skill is missing"
+            }
+        )
+    return filter_by_skill
